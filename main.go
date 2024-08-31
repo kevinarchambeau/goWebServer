964504a95d2cd5e7a1d8b1171db2b3b52fb5149e
kevinarchambeau <xxerexx@gmail.com>
2024-08-31 16:43:50 -0700

    clean up, changed to use handle func

--- main.go
+++ main.go
@@ -11,29 +11,21 @@ type Server struct {
 	Handler http.ServeMux
 }
 
-type routes struct {
-}
-
 func main() {
 	mux := http.NewServeMux()
 	serverConfig := Server{
 		Addr: ":8080",
 	}
-	healthz := routes{}
 	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("./"))))
 	mux.Handle("app/assets/", http.StripPrefix("/app", http.FileServer(http.Dir("./assets/"))))
-	mux.Handle("/healthz", healthz)
-	//mux.HandleFunc("/app", app)
-	//mux.Handle("/app", http.FileServer(http.Dir(".")))
-
-	//mux.Handle("/app/assets/", http.FileServer(http.Dir("./assets/")))
+	mux.HandleFunc("/healthz", healthz)
 	err := http.ListenAndServe(serverConfig.Addr, mux)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 	}
 }
 
-func (healthz routes) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func healthz(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	io.WriteString(w, "OK")
