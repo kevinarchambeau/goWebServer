3744896e31f7b463fb1de03b06db14c14afa9646
kevinarchambeau <xxerexx@gmail.com>
2024-08-31 16:40:32 -0700

    updated to use new routers and handler

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -10,15 +11,30 @@ type Server struct {
 	Handler http.ServeMux
 }
 
+type routes struct {
+}
+
 func main() {
 	mux := http.NewServeMux()
 	serverConfig := Server{
 		Addr: ":8080",
 	}
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	mux.Handle("assets/", http.FileServer(http.Dir("./assets/")))
+	healthz := routes{}
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("./"))))
+	mux.Handle("app/assets/", http.StripPrefix("/app", http.FileServer(http.Dir("./assets/"))))
+	mux.Handle("/healthz", healthz)
+	//mux.HandleFunc("/app", app)
+	//mux.Handle("/app", http.FileServer(http.Dir(".")))
+
+	//mux.Handle("/app/assets/", http.FileServer(http.Dir("./assets/")))
 	err := http.ListenAndServe(serverConfig.Addr, mux)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 	}
+}
+
+func (healthz routes) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	io.WriteString(w, "OK")
 }
