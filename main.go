abb5d497148fae36aa962c0617c581268b80de28
kevinarchambeau <xxerexx@gmail.com>
2024-09-01 12:47:46 -0700

    added validate_chirp endpoint

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 )
 
@@ -23,10 +25,12 @@ func main() {
 	apiCfg := apiConfig{
 		fileserverHits: 0,
 	}
+
 	mux.Handle("GET /app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("./")))))
+	mux.HandleFunc("GET /admin/metrics", apiCfg.getCount)
 	mux.HandleFunc("GET /api/healthz", healthz)
 	mux.HandleFunc("GET /api/reset", apiCfg.resetCount)
-	mux.HandleFunc("GET /admin/metrics", apiCfg.getCount)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirp)
 	err := http.ListenAndServe(serverConfig.Addr, mux)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
@@ -59,4 +63,55 @@ func (cfg *apiConfig) getCount(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, template, cfg.fileserverHits)
+}
+
+func validateChirp(w http.ResponseWriter, req *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	type errorReturnVals struct {
+		Error string `json:"error"`
+	}
+
+	type returnVals struct {
+		Valid bool `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	if len(params.Body) > 140 {
+		responseBody := errorReturnVals{
+			Error: "message is too long",
+		}
+		data, err := json.Marshal(responseBody)
+		if err != nil {
+			log.Printf("Error marshalling: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(data)
+		return
+	}
+	responseBody := returnVals{
+		Valid: true,
+	}
+	data, err := json.Marshal(responseBody)
+	if err != nil {
+		log.Printf("Error marshalling: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
 }
