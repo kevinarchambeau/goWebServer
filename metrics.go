665c7a836a56bfa6e1286c57697fcad567b2b5ca
kevinarchambeau <xxerexx@gmail.com>
2024-09-02 11:21:45 -0700

    minor refactor

--- metrics.go
+++ metrics.go
@@ -5,24 +5,24 @@ import (
 	"net/http"
 )
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+func (apiCfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	// need to do it like this, so the middleware is executed then it's served
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		cfg.fileserverHits++
+		apiCfg.fileserverHits++
 		next.ServeHTTP(w, req)
 	})
 }
 
-func (cfg *apiConfig) resetCount(w http.ResponseWriter, req *http.Request) {
-	cfg.fileserverHits = 0
+func (apiCfg *apiConfig) resetCount(w http.ResponseWriter, req *http.Request) {
+	apiCfg.fileserverHits = 0
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Count reset"))
 }
 
-func (cfg *apiConfig) getCount(w http.ResponseWriter, req *http.Request) {
+func (apiCfg *apiConfig) getCount(w http.ResponseWriter, req *http.Request) {
 	template := "<html>\n\n<body>\n    <h1>Welcome, Chirpy Admin</h1>\n    <p>Chirpy has been visited %d times!</p>\n</body>\n\n</html>"
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, template, cfg.fileserverHits)
+	fmt.Fprintf(w, template, apiCfg.fileserverHits)
 }
